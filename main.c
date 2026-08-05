0c4a08c17971e8e372db1e1c5e0b6a06a76dd480
Ash <ashsmuldoon@gmail.com>
2026-08-05 19:17:02 -0400

    Sorted the xor logic

--- main.c
+++ main.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv) {
-  if argc == 0 { pritnf("Please include arguments. ./otp-cheat < -i to create a random string of characters || -o to create a key that matches a string of characters to any message> <input> <message>"); return 1; }
-
-  char **arguments = malloc(argc * sizeof(char *));
-
-  if sizeof(arguments[1]) != sizeof(arguments[2]) { return 1; }
-
-  for (int i = 0; i < argc; i++) {
-    arguments[i] = argv[i]; 
+int main(int argc, char **argv) {
+  // take in argv, expected argc of two
+  if (argc != 3) {
+    fprintf(stderr, "requires two strings of equal length.");
+    return EXIT_FAILURE;
   }
 
+  // compare argv[1] to argc[2] (must be the same size)
+  size_t len1 = strlen(argv[1]);
+  size_t len2 = strlen(argv[2]);
 
+  if (len1 != len2) {
+     fprintf(stderr, "requires two strings of equal length.\n");
+     return EXIT_FAILURE;
+  }
 
-  free(arguments); 
+  // xor them together
+  char *result = malloc(len1+1);
+  if (!result) {
+    fprintf(stderr, "malloc fail");
+    return EXIT_FAILURE;
+  }
+
+  for (size_t i = 0; i < len1; i++) {
+    result[i] = argv[1][i] ^ argv[2][i];
+  }
+
+  result[len1] = '\0';
+
+  // return the output
+
+  for (size_t i = 0; i<len1; i++) {
+    printf("%02x", (unsigned char)result[i]);
+  }
+
+  printf("\n");
+  free(result);
+
   return 0;
-}
-
-void xorTwoStrings(const char *s1, const char *s2) {
-  
-
 }
